3355b7f3483a2e534fffbb291dc33e0180182a3a
Harun Khan <harun19@stanford.edu>
2024-07-24 03:15:04 -0700

    all sorries proved

--- Smt/Reconstruction/Certifying/Arith/TransFns/ArithTransApproxAboveBelow.lean
+++ Smt/Reconstruction/Certifying/Arith/TransFns/ArithTransApproxAboveBelow.lean
@@ -37,10 +37,6 @@ theorem deriv_comp_mul {f : Real → Real} (hd : Differentiable Real f) :
   simp
 
 
-#check ENat.coe_ne_top
-example (a: Nat) : a < (⊤ : ℕ∞) := by
-  apply Ne.lt_top (ENat.top_ne_coe a).symm
-
 theorem iteratedDeriv_const_mul {f : ℝ → ℝ } (d : Nat) (c : Real) (hf : ContDiff Real ⊤ f):
   ∀ x, iteratedDeriv d (fun x => f (c*x)) x = c^d * (iteratedDeriv d f (c*x)) := by
   induction' d with d ih
@@ -50,8 +46,10 @@ theorem iteratedDeriv_const_mul {f : ℝ → ℝ } (d : Nat) (c : Real) (hf : Co
     rw [congrArg deriv (funext ih)]
     rw [deriv_const_mul, deriv_comp_mul, ← iteratedDeriv_succ, pow_succ', ← mul_assoc]
     apply ContDiff.differentiable_iteratedDeriv d hf (Ne.lt_top (ENat.top_ne_coe d).symm)
-    sorry
-
+    apply Differentiable.differentiableAt
+    rw [show (fun x => iteratedDeriv d f (c * x)) = ((iteratedDeriv d f) ∘ (fun x => c*x)) by rfl]
+    apply Differentiable.comp (ContDiff.differentiable_iteratedDeriv d ((contDiff_top.mp hf) (d+1)) (by norm_cast; simp))
+    apply Differentiable.const_mul (differentiable_id)
 
 lemma taylorCoeffWithin_neg (d n : ℕ) (hf : ContDiff Real ⊤ f):
   taylorCoeffWithin f d Set.univ x₀ = (-1 : Real)^d * taylorCoeffWithin (fun x => f (-x)) d Set.univ (-x₀) := by
@@ -82,9 +80,6 @@ theorem derivWithin_eq_deriv {f : Real → Real} (hf : ContDiff Real ⊤ f) (hs 
   simp only [derivWithin, deriv]
   rw [fderivWithin_eq_fderiv hs (ContDiffAt.differentiableAt (ContDiff.contDiffAt hf) (by simp))]
 
-#check ContDiffOn.differentiableOn
-#check Set.EqOn.iteratedFDerivWithin
-#check UniqueDiffOn.uniqueDiffWithinAt
 
 theorem iteratedDerivWithin_eq_iteratedDeriv {f : Real → Real} (hf : ContDiff Real ⊤ f) (hs : UniqueDiffOn Real s):
   ∀ x ∈ s, iteratedDerivWithin d f s x = iteratedDeriv d f x := by
@@ -94,9 +89,7 @@ theorem iteratedDerivWithin_eq_iteratedDeriv {f : Real → Real} (hf : ContDiff 
     rw [iteratedDerivWithin_succ (UniqueDiffOn.uniqueDiffWithinAt hs hx), iteratedDeriv_succ, derivWithin, deriv]
     rw [fderivWithin_congr hd (hd x hx)]
     rw [fderivWithin_eq_fderiv (UniqueDiffOn.uniqueDiffWithinAt hs hx)]
-    sorry
-    -- apply DifferentiableOn.differentiableAt (ContDiffOn.differentiableOn (by sorry) (by sorry))
-
+    apply Differentiable.differentiableAt (ContDiff.differentiable_iteratedDeriv d ((contDiff_top.mp hf) (d + 1)) (by norm_cast; simp))
 
 -- This should be generalized
 theorem taylorCoeffWithin_eq {f : Real → Real} (s : Set Real) (hx : x₀ ∈ s) (hs : UniqueDiffOn ℝ s) (hf : ContDiff Real ⊤ f):
@@ -113,16 +106,25 @@ theorem taylorWithinEval_eq {f : Real → Real} (s : Set Real) (hs : x₀ ∈ s)
 
 theorem taylor_mean_remainder_lagrange₁ {f : ℝ → ℝ} {x x₀ : ℝ} {n : ℕ} (hx : x < x₀)
   (hf : ContDiff ℝ ⊤ f)
-  (hf' : DifferentiableOn ℝ (iteratedDerivWithin n f (Icc x x₀)) (Ioo x x₀)) :
-  ∃ (x' : ℝ) (_ : x' ∈ Ioo x₀ x), f x - taylorWithinEval f n (Icc x x₀) x₀ x =
+  :
+  ∃ (x' : ℝ) (_ : x' ∈ Ioo x x₀), f x - taylorWithinEval f n (Icc x x₀) x₀ x =
     iteratedDerivWithin (n + 1) f (Icc x x₀) x' * (x - x₀) ^ (n + 1) / (n + 1)! := by
-  have ⟨x' , hx', H⟩:= taylor_mean_remainder_lagrange (f := fun x => f (-x)) (n := n) (neg_lt_neg hx) (by sorry) (by sorry)
-  use -x'
-  use (show -x' ∈ Ioo x₀ x by sorry)
-  rw [neg_neg, taylorWithinEval_eq _ sorry] at H
-  rw [taylorWithinEval_neg, ←taylorWithinEval_eq (Icc x x₀)] at H
+  have H1 := (show (f ∘ (fun x => -x)) = (fun p => f (-p)) by rfl) ▸ ContDiff.comp hf contDiff_neg
+  have H2 : DifferentiableOn ℝ (iteratedDerivWithin n (fun x => f (-x)) (Icc (-x₀) (-x))) (Ioo (-x₀) (-x)) := by
+    apply DifferentiableOn.mono _ Set.Ioo_subset_Icc_self
+    apply ContDiffOn.differentiableOn_iteratedDerivWithin (n := n + 1) _ (by norm_cast; simp) (uniqueDiffOn_Icc (neg_lt_neg hx))
+    apply ContDiff.contDiffOn ((contDiff_top.mp H1) _)
+  have ⟨x' , hx', H⟩:= taylor_mean_remainder_lagrange (f := fun x => f (-x)) (n := n) (neg_lt_neg hx) (ContDiff.contDiffOn ((contDiff_top.mp H1) n)) H2
+  have hx'' : -x' ∈ Ioo x x₀ := by
+    simp at *
+    apply And.intro
+    <;> apply neg_lt_neg_iff.mp
+    <;> simp [hx']
+  use -x'; use hx''
+  rw [neg_neg, taylorWithinEval_eq _ (by simp [le_of_lt hx]) (uniqueDiffOn_Icc (by simp [hx])) H1] at H
+  rw [taylorWithinEval_neg H1, ←taylorWithinEval_eq (Icc x x₀) (by simp [le_of_lt hx]) (uniqueDiffOn_Icc hx) (by simp [hf])] at H
   simp only [neg_neg, sub_neg_eq_add] at H
-  rw [H, iteratedDerivWithin_eq_iteratedDeriv, iteratedDerivWithin_eq_iteratedDeriv]
+  rw [H, iteratedDerivWithin_eq_iteratedDeriv hf (uniqueDiffOn_Icc (by simp [hx])) _ (Set.Ioo_subset_Icc_self hx''), iteratedDerivWithin_eq_iteratedDeriv H1 (uniqueDiffOn_Icc (by simp [hx])) _ (Set.Ioo_subset_Icc_self hx')]
   rw [show (fun x => f (-x)) = (fun x => f (-1 * x)) by simp]
-  rw [iteratedDeriv_const_mul _ _ hf, mul_rotate, mul_assoc, ← mul_pow, add_comm (-x) x₀]
+  rw [iteratedDeriv_const_mul _ _ hf, mul_rotate, mul_assoc, ←mul_pow, add_comm (-x) x₀]
   simp [sub_eq_add_neg]
